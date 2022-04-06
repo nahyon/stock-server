ba9042a78c420cda50976c243e5cd18368a44c23
nahyon <62339370+nahyon@users.noreply.github.com>
2022-04-06 16:20:48 +0900

    DB modify

--- DB/1. create table(daily, intraday).sql
+++ DB/1. create table(daily, intraday).sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS daily (symbol VARCHAR(50), timestamp DATE NOT NULL, o
 
 CREATE TABLE IF NOT EXISTS intraday (symbol VARCHAR(50), timestamp DATETIME NOT NULL, open DECIMAL(10,4) NOT NULL, high DECIMAL(10,4) NOT NULL, low DECIMAL(10,4) NOT NULL, close DECIMAL(10,4) NOT NULL, volume INT NOT NULL, constraint daily_PK primary key(symbol, timestamp));
 
-CREATE TABLE IF NOT EXISTS company_info(symbol VARCHAR(10), id INT AUTO_INCREMENT, name_en VARCHAR(50), name_kr VARCHAR(50), desc_en TEXT, desc_kr TEXT, updatedAt_daily DATE, updatedAt_intraday DATETIME, imgurl BLOB, constraint company_info_PK primary key(id, symbol));
+CREATE TABLE IF NOT EXISTS company_info(symbol VARCHAR(10), id INT AUTO_INCREMENT, name_en VARCHAR(50), name_kr VARCHAR(50), desc_en TEXT, desc_kr TEXT, img TEXT, shareout INT, updatedAt_daily DATE, updatedAt_intraday DATETIME, constraint company_info_PK primary key(id, symbol));
